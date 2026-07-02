e607aff3e783b9205be18a3563fdeef33b373186
Mide_xol <okunolaolamide7@gmail.com>
2026-07-02 11:16:54 +0100

    feat(contract): define basic PayoutItem structure

--- contracts/hedgepay_batch/src/lib.rs
+++ contracts/hedgepay_batch/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![no_std]
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Address, Env, Symbol, Vec, panic_with_error};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct PayoutItem {
+    pub payee: Address,
+    pub amount: i128,
+    pub department: Symbol,
 }
